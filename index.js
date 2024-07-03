d12bfff8eca64fb948531ccf4194a384bb1fd0d8
cawzkf <camilafelixdosreis@gmail.com>
2024-07-03 11:24:02 +0000

    Inicial

--- index.js
+++ index.js
@@ -2,21 +2,11 @@ const express = require ('express')
 const app = express()
 
 app.get('/', function (req, res){
-
-  const frutas = ['Laranja', 'Maçã','Banana', 'Mamão'];
-
-  let listafrutas = 'Frutas: <br>';
-
-  frutas.forEach((fruta,index) => {
-
-    listafrutas += `${index + 1}. ${fruta} <br>`;
-
-  });
-
-  res.status(200).send(listafrutas);
+  res.send('Hello, world')
 })
 
-app.listen(3000,function(){
-  console.log('Servidor rodando na porta 3000');
-
+app.get('/oi', function (req, res){
+  res.send('Olá, mundo')
 })
+
+app.listen(3000)
